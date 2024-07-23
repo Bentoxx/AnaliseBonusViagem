5db4808f281c519a45994b5cb832b85330c52843
Bento Netto <bentoeemanuele@gmail.com>
2024-07-23 09:51:48 -0300

    Recriação do Diretório

--- AnaliseBonusViagem/main.py
+++
@@ -1,9 +0,0 @@
-#Passo a passo de solução
-
-#Abrir os 6 arquivos em Excel   
-
-#Para cada arquivo:
-
-    #Verificar se algum valor na coluna de vendas é maior do que R$ 55.000,00
-    #Se for maior do que R$ 55.000,00 -> Envia um sms com o Nome do Vendedor, o mês e o valor alcançado
-    
