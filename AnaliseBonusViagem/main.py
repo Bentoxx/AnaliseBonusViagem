30063bdf111bea063db570a78691bdba84be7f8d
Bento Netto <bentoeemanuele@gmail.com>
2024-07-23 10:29:34 -0300

    Adicionadas as bibliotecas Pandas, Twilio e Openpyxl

---
+++ AnaliseBonusViagem/main.py
@@ -0,0 +1,7 @@
+#Passo a passo da aplicação
+
+#Abrir os 6 arquivos em Excel
+
+#Para cada arquivo:
+    #Verificar se algum valor da coluna de vendas > R$ 55.000,00
+    #Se > R$ 55.000,00 -> enviar um SMS com o nome do vendedor, o mês e o valor de vendido
